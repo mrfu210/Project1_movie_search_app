7f74ef515f25011371d15c033c4dfe4aa69eabcc
mrfu210 <scutaru.georgian@yahoo.ro>
2021-10-11 21:54:49 +0100

    Add search box + run the app

---
+++ src/test.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Test(){
+
+    return(
+        <h1>Hello World</h1>
+    )
+}
+
+//to create the SearchMovies component
+//form with a class of form
+//label wit htmlFor="query" ad a class of Label
+//input of type text with a name of "query" and a placeholder
+//butto class of button and a type of submit
